bf2210b84c54693ae4eb7512950ea885239409fc
Unknown <maxime.heron@outlook.com>
2019-02-11 14:17:46 +0100

    Question 2
    lecture repertoire recursive

--- src/fr/unice/miage/heron/tp1/Main.java
+++ src/fr/unice/miage/heron/tp1/Main.java
@@ -4,14 +4,23 @@ import java.io.File;
 
 public class Main {
 
-	public static void main(String[] args) {
-		File folder = new File("../");
-		String[] file = folder.list();
-		for(int i =0; i < file.length; i++) {
-			System.out.println(file[i]);
+	public static void readFolder(File folder) {
+		
+			for(int i =0; i < folder.list().length; i++) {
+				System.out.println(folder.listFiles()[i]);
+				if(folder.listFiles()[i].isDirectory()) {
+					readFolder(folder.listFiles()[i]);
+				}
+			}
+			
+			
 		}
+	
+	public static void main(String[] args) {
 		
 		
-	}
+		File folder = new File("../");
+		readFolder(folder);
 
+}
 }
